830ea39e3f1d4d1010c44dbb4ce90a07afbc42a9
Sricharan Vagvala <charansv19@gmail.com>
2026-03-21 16:11:17 -0700

    Program finished!

--- data/run.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-g++ -Wall -Wextra --std=c++17 main.cpp -o main.exe
-
-Get-Content data/data1.txt | .\main.exe > result1.txt
-Get-Content data/data2.txt | .\main.exe > result2.txt
-Get-Content data/data3.txt | .\main.exe > result3.txt
-Get-Content data/data4.txt | .\main.exe > result4.txt
--- data/run.sh
+++ data/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-g++ -Wall -Wextra --std=c++17 main.cpp -o main 
+g++ -Wall -Wextra --std=c++17 main.cpp -o main
 timeout 10 ./main < data/data1.txt > result1.txt
 timeout 10 ./main < data/data2.txt > result2.txt
 timeout 10 ./main < data/data3.txt > result3.txt
