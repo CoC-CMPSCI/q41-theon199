fbb5c7455529b3f55081bacc26fbc6b4396f2b9d
Sricharan Vagvala <charansv19@gmail.com>
2026-03-21 16:17:38 -0700

    Add .gitattributes file and remove timeout from run.sh script

--- data/run.sh
+++ data/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 g++ -Wall -Wextra --std=c++17 main.cpp -o main
-timeout 10 ./main < data/data1.txt > result1.txt
-timeout 10 ./main < data/data2.txt > result2.txt
-timeout 10 ./main < data/data3.txt > result3.txt
-timeout 10 ./main < data/data4.txt > result4.txt
+./main < data/data1.txt > result1.txt
+./main < data/data2.txt > result2.txt
+./main < data/data3.txt > result3.txt
+./main < data/data4.txt > result4.txt
